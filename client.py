c6ea0bedafac7545572c07fd3220486a0a462d1d
HappyShoelace <bboldeman@gmail.com>
2023-06-06 17:33:33 +1000

    Django Experimentation

---
+++ client.py
@@ -0,0 +1,9 @@
+ID = "13a615a80705420f9bd7c26cc1c2d2d9"
+Secret = "fdea8067c26a4d73873632e1bd0a6302"
+
+
+dbuser = "postgres"
+dbpass = "password"
+dbHost = "127.0.0.1"
+dbPort = "5432"
+dbName = "MusicMate"
